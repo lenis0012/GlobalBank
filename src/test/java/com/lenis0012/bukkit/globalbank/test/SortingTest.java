408c36bf3b491a6ef433bbc7098f61a1732b6fcd
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-07-15 11:51:16 +0200

    Added sorting test

---
+++ src/test/java/com/lenis0012/bukkit/globalbank/test/SortingTest.java
@@ -0,0 +1,23 @@
+package com.lenis0012.bukkit.globalbank.test;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Lenny on 7/15/2014.
+ */
+public class SortingTest {
+
+    @Test
+    public void testAlphabeticSorting() {
+        ItemStack[] items = new ItemStack[4];
+        items[0] = new ItemStack(Material.LEATHER, 1);
+        items[2] = new ItemStack(Material.ANVIL, 1);
+        Assert.assertTrue(items[2] == null);
+        Assert.assertTrue(items[3] == null);
+        Assert.assertTrue(items[0].getType() == Material.ANVIL);
+        Assert.assertTrue(items[1].getType() == Material.LEATHER);
+    }
+}
