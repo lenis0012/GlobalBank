02cc46aa839ee3995653291b6eaedd84c362fe48
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-07-15 13:21:04 +0200

    click listener

--- src/main/java/com/lenis0012/bukkit/globalbank/BankListener.java
+++ src/main/java/com/lenis0012/bukkit/globalbank/BankListener.java
@@ -5,16 +5,19 @@ import com.lenis0012.bukkit.globalbank.storage.BPlayer;
 import com.lenis0012.bukkit.npc.NPC;
 import com.lenis0012.bukkit.npc.NPCFactory;
 import com.lenis0012.bukkit.npc.NPCInteractEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by Lenny on 7/14/2014.
@@ -43,6 +46,30 @@ public class BankListener implements Listener {
     }
 
     @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        final Player player = (Player) event.getWhoClicked();
+        final ItemStack item = event.getCurrentItem();
+        final BPlayer bPlayer = BPlayer.get(player.getUniqueId());
+        if(bPlayer.getStatus() == BPlayer.PlayerStatus.IN_BANK) {
+            event.setCancelled(true);
+            switch(item.getType()) {
+                case CHEST:
+                    final int slot = Integer.parseInt(item.getItemMeta().getDisplayName().substring("Slot ".length())) - 1;
+                    player.closeInventory();
+                    Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+                        @Override
+                        public void run() {
+                            bPlayer.openSlot(player, slot);
+                        }
+                    }, 2L);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         final Player player = (Player) event.getPlayer();
         final Inventory inventory = event.getInventory();
@@ -50,6 +77,9 @@ public class BankListener implements Listener {
         if(bPlayer.getStatus() == BPlayer.PlayerStatus.IN_SLOT) {
             int slot = Integer.parseInt(inventory.getTitle().substring("Slot ".length())) - 1;
             bPlayer.closeSlot(event.getInventory(), slot);
+            bPlayer.setStatus(BPlayer.PlayerStatus.NONE);
+        } else if(bPlayer.getStatus() == BPlayer.PlayerStatus.IN_BANK) {
+            bPlayer.setStatus(BPlayer.PlayerStatus.NONE);
         }
     }
 
