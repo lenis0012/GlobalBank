71ec23df3f9a9d4874ebda52597302b8c8727939
DarkShimy00 <darkshimy00@gmail.com>
2014-07-30 19:02:53 +0100

    Change the way items are sorted to support enchant
    ments, durability and things that are linked with the ItemStack. For doing that, we keep a copy of the ItemStacks during the process to ensure no loss of data.
    
    It also changes how the plugin handle ItemStacks larger than the MaxStackSize: larger ItemStacks won't be split into multiple stacks. By changing this, we fix an error that would occur (not tested) if the items are a lot larger by causing the contents[] array to be larger than the menu.

--- src/main/java/com/lenis0012/bukkit/globalbank/util/sorting/AlphabeticSort.java
+++ src/main/java/com/lenis0012/bukkit/globalbank/util/sorting/AlphabeticSort.java
@@ -1,11 +1,17 @@
 package com.lenis0012.bukkit.globalbank.util.sorting;
 
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by Lenny on 7/14/2014.
@@ -36,29 +42,85 @@ public class AlphabeticSort implements ISort {
         int index = 0;
 
         List<ItemStack> itemList = Arrays.asList(items);
-        for(Material mat : alphabeticMaterialList) {
-            ItemStack current = new ItemStack(mat, 0);
-            for(ItemStack it : itemList) {
-                if(it != null && it.getType().equals(mat)) {
-                    if (current.getAmount() + it.getAmount() > mat.getMaxStackSize()) {
-                        int remaining = mat.getMaxStackSize() - current.getAmount();
-                        current.setAmount(mat.getMaxStackSize());
-                        contents[index] = current;
-                        index += 1;
+        LinkedHashMap<ItemStack, LinkedList<Integer>> itemMap = new LinkedHashMap<ItemStack, LinkedList<Integer>>();
+        Map<ItemStack, Map<Integer, ItemStack>> itemStackMap = new HashMap<ItemStack, Map<Integer, ItemStack>>();
 
-                        current = new ItemStack(mat, it.getAmount() - remaining);
+        for(Material mat : alphabeticMaterialList) {
+            for(ItemStack it : itemList) {
+                if(it != null && it.getType().equals(mat) && it.getAmount() > 0) {
+                    LinkedList<Integer> currents;
+                    if(itemMap.containsKey(getEmptyItemStack(it))) {
+                        currents = itemMap.get(getEmptyItemStack(it));
                     } else {
-                        current.setAmount(current.getAmount() + it.getAmount());
+                        currents = new LinkedList<Integer>();
                     }
+
+                    if(currents.size() == 0 || it.getAmount() >= mat.getMaxStackSize() ||currents.getLast() >= mat.getMaxStackSize()) {
+                        currents.add(it.getAmount());
+                    } else {
+                        int remaining;
+
+                        if(currents.getLast() + it.getAmount() > mat.getMaxStackSize()) {
+                            remaining = currents.getLast() + it.getAmount();
+                            currents.removeLast();
+
+                            do {
+                                remaining = remaining - mat.getMaxStackSize();
+
+                                currents.add(mat.getMaxStackSize());
+                            } while(remaining > mat.getMaxStackSize());
+                        } else {
+                            remaining = currents.getLast() + it.getAmount();
+                            currents.removeLast();
+                        }
+
+                        if(remaining > 0) {
+                            currents.add(remaining);
+                        }
+                    }
+
+                    itemMap.put(getEmptyItemStack(it), currents);
+                    Map<Integer, ItemStack> sizes = itemStackMap.get(getEmptyItemStack(it));
+
+                    if(sizes == null) {
+                        sizes = new HashMap<Integer, ItemStack>();
+                    }
+
+                    sizes.put(it.getAmount(), it);
+                    itemStackMap.put(getEmptyItemStack(it), sizes);
                 }
             }
+        }
 
-            if(current.getAmount() > 0) {
-                contents[index] = current;
-                index += 1;
+        for(Entry<ItemStack, LinkedList<Integer>> entry : itemMap.entrySet()) {
+            LinkedList<Integer> currents = entry.getValue();
+
+            Collections.sort(currents);
+            Collections.reverse(currents);
+
+            for(int amount : currents) {
+                Map<Integer, ItemStack> sizes = itemStackMap.get(getEmptyItemStack(entry.getKey()));
+
+                if(sizes.containsKey(amount)) {
+                    contents[index] = sizes.get(amount);
+                } else {
+                    entry.getKey().setAmount(amount);
+                    contents[index] = entry.getKey();
+                }
+
+                index++;
             }
         }
 
         return contents;
+    }
+
+    public ItemStack getEmptyItemStack(ItemStack item) {
+        ItemStack emptyItemStack = new ItemStack(item);
+
+        // Not really empty but setting amount to 0 causes Bukkit to change the material to AIR.
+        emptyItemStack.setAmount(1);
+
+        return emptyItemStack;
     }
 }
